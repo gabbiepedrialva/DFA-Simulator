c4ad63964ae92c31726dff6be6a3c457cc176364
Gabrielle Elyssa Pedrialva <126461787+gabbiepedrialva@users.noreply.github.com>
2024-05-12 01:57:29 +0800

    Update Logic.js
    Temporary logic (with changes to fit the logic of both DFAs)

--- components/DFA/Logic.js
+++ components/DFA/Logic.js
@@ -19,55 +19,36 @@ export class DFA {
   }
   node() {
     this.currentInputPos += 1;
-    if (this.currentInputPos == "T") {
-      this.result = "Invalid";
-      // console.log("Invalid String TRAP");
-      // console.log("Path Taken:", this.path);
+    if (this.currentInputPos === this.input.length) {
+        if (this.input.length === 0 || (
+            (this.problem === problem1 && (this.currentNode === 14 || this.currentNode === 15)) ||
+            (this.problem === problem2 && this.currentNode === 9)
+        )) {
+            this.result = "Valid";
+        } else {
+            this.result = "Invalid";
+        }
     } else {
-      if (
-        this.currentNode < this.problem.length &&
-        this.input[this.currentInputPos] != undefined
-      ) {
-        let node = this.problem[this.currentNode - 1];
-        // console.log(this.path, this.input[this.currentInputPos]);
-
         if (
-          this.input[this.currentInputPos] == "a" ||
-          this.input[this.currentInputPos] == "b" ||
-          this.input[this.currentInputPos] == "0" ||
-          this.input[this.currentInputPos] == "1"
+            this.currentNode <= this.problem.length &&
+            this.input[this.currentInputPos] !== undefined
         ) {
-          this.currentNode =
-            node.direction[
-              this.language.indexOf(this.input[this.currentInputPos])
-            ];
-          // console.log(`currentNode: ${this.currentNode}`);
-          // console.log(
-          //   `node.direction[]: ${this.language.indexOf(
-          //     this.input[this.currentInputPos]
-          //   )}`
-          // );
-          this.currentNode != undefined && this.path.push(this.currentNode);
+            let node = this.problem[this.currentNode - 1];
+
+            if (
+                (this.problem === problem1 && (this.input[this.currentInputPos] === "a" || this.input[this.currentInputPos] === "b")) ||
+                (this.problem === problem2 && (this.input[this.currentInputPos] === "0" || this.input[this.currentInputPos] === "1"))
+            ) {
+                this.currentNode = node.direction[this.language.indexOf(this.input[this.currentInputPos])];
+                this.currentNode !== undefined && this.path.push(this.currentNode);
+            } else {
+                this.currentNode = "T";
+            }
+            this.node();
         } else {
-          this.currentNode = "T";
-          // console.log(
-          //   "String contains a letter not in the language - ",
-          //   this.language
-          // );
+            this.result = "Invalid";
+            this.path.push("eos");
         }
-        this.node();
-      } else {
-        if (this.currentNode == this.problem.length) {
-          this.result = "Valid";
-          // console.log("Valid String");
-          // console.log("Path Taken", this.path);
-        } else {
-          this.result = "Invalid";
-          // console.log("Invalid String SHORT");
-          this.path.push("eos");
-          // console.log("Path Taken", this.path);
-        }
-      }
     }
   }
 }
@@ -91,15 +72,15 @@ export const problem1 = [
 ];
 
 export const problem2 = [
-  new Node(1, 3, 4),
-  new Node(2, 2, 2),
-  new Node(3, 5, 4),
-  new Node(4, 3, 5),
-  new Node(5, 6, 6),
-  new Node(6, 6, 7),
-  new Node(7, 8, 9),
-  new Node(8, 6, 2),
-  new Node(9, 8, 2),
+  new Node(1, 2, 3),
+  new Node(2, 4, 3),
+  new Node(3, 2, 4),
+  new Node(4, 5, 5),
+  new Node(5, 5, 6),
+  new Node(6, 7, 8),
+  new Node(7, 5, 9),
+  new Node(8, 7, 9),
+  new Node(9, 9, 9),
 ];
 
 export const language1 = ["a", "b"];
