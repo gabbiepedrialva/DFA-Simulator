2a4aa35d0af3dc6c14abdacbeba3f03d52ecc38c
Gabrielle Elyssa Pedrialva <126461787+gabbiepedrialva@users.noreply.github.com>
2024-05-12 16:45:01 +0800

    Finalizing Logic.js

--- components/DFA/Logic.js
+++ components/DFA/Logic.js
@@ -19,56 +19,37 @@ export class DFA {
   }
   node() {
     this.currentInputPos += 1;
-    if (this.currentInputPos == "T") {
-      this.result = "Invalid";
-      // console.log("Invalid String TRAP");
-      // console.log("Path Taken:", this.path);
-    } else {
-      if (
-        this.currentNode < this.problem.length &&
-        this.input[this.currentInputPos] != undefined
-      ) {
-        let node = this.problem[this.currentNode - 1];
-        // console.log(this.path, this.input[this.currentInputPos]);
-
+    if (this.currentInputPos === this.input.length) {
         if (
-          this.input[this.currentInputPos] == "a" ||
-          this.input[this.currentInputPos] == "b" ||
-          this.input[this.currentInputPos] == "0" ||
-          this.input[this.currentInputPos] == "1"
+            (this.problem === problem1 && (this.currentNode === 14 || this.currentNode === 15)) ||
+            (this.problem === problem2 && this.currentNode === 9)
         ) {
-          this.currentNode =
-            node.direction[
-              this.language.indexOf(this.input[this.currentInputPos])
-            ];
-          // console.log(`currentNode: ${this.currentNode}`);
-          // console.log(
-          //   `node.direction[]: ${this.language.indexOf(
-          //     this.input[this.currentInputPos]
-          //   )}`
-          // );
-          this.currentNode != undefined && this.path.push(this.currentNode);
+            this.result = "Valid";
         } else {
-          this.currentNode = "T";
-          // console.log(
-          //   "String contains a letter not in the language - ",
-          //   this.language
-          // );
+            this.result = "Invalid";
         }
-        this.node();
-      } else {
-        if (this.currentNode == this.problem.length) {
-          this.result = "Valid";
-          // console.log("Valid String");
-          // console.log("Path Taken", this.path);
+    } else {
+        if (
+          this.currentNode <= this.problem.length && this.currentNode !== "T1" && this.currentNode !== "T2" &&
+          this.input[this.currentInputPos] !== undefined
+        ) {
+            let node = this.problem[this.currentNode - 1];
+
+            if (
+                (this.problem === problem1 && (this.input[this.currentInputPos] === "a" || this.input[this.currentInputPos] === "b")) ||
+                (this.problem === problem2 && (this.input[this.currentInputPos] === "0" || this.input[this.currentInputPos] === "1"))
+            ) {
+                this.currentNode = node.direction[this.language.indexOf(this.input[this.currentInputPos])];
+                this.currentNode !== undefined && this.path.push(this.currentNode);
+            } else {
+              this.currentNode = (this.problem === problem1 ? "T1" : "T2");
+            }
+            this.node();
         } else {
-          this.result = "Invalid";
-          // console.log("Invalid String SHORT");
-          this.path.push("eos");
-          // console.log("Path Taken", this.path);
+            this.result = "Invalid";
+            this.path.push("eos");
         }
       }
-    }
   }
 }
 
@@ -76,11 +57,11 @@ export const problem1 = [
   new Node(1, 2, 3),
   new Node(2, 4, 5),
   new Node(3, 6, 5),
-  new Node(4, "T", 7),
-  new Node(5, 8, "T"),
-  new Node(6, "T", 9),
-  new Node(7, 10, "T"),
-  new Node(8, "T", 10),
+  new Node(4, "T1", 7),
+  new Node(5, 8, "T1"),
+  new Node(6, "T2", 9),
+  new Node(7, 10, "T1"),
+  new Node(8, "T2", 10),
   new Node(9, 10, 3),
   new Node(10, 11, 11),
   new Node(11, 12, 13),
@@ -91,15 +72,15 @@ export const problem1 = [
 ];
 
 export const problem2 = [
-  new Node(1, 3, 4),
-  new Node(2, 2, 2),
-  new Node(3, 5, 4),
-  new Node(4, 3, 5),
-  new Node(5, 6, 6),
-  new Node(6, 6, 7),
-  new Node(7, 8, 9),
-  new Node(8, 6, 2),
-  new Node(9, 8, 2),
+  new Node(1, 2, 3),
+  new Node(2, 4, 3),
+  new Node(3, 2, 4),
+  new Node(4, 5, 5),
+  new Node(5, 5, 6),
+  new Node(6, 7, 8),
+  new Node(7, 5, 9),
+  new Node(8, 7, 9),
+  new Node(9, 9, 9),
 ];
 
 export const language1 = ["a", "b"];
